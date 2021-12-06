bde7836f8d82be79124e9b7bb40372038c665cc8
Javlonbek Rajabov <radjabov377@mail.ru>
2021-12-06 20:28:22 +0500

    init

--- YouTube_Downloader/Program.cs
+++ YouTube_Downloader/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Diagnostics;
+using VideoLibrary;
+using static System.Console;
+
 
 namespace YouTube_Downloader
 {
@@ -6,7 +11,47 @@ namespace YouTube_Downloader
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            if (args.Length == 2)
+            {
+                if (isYouTubeVideoLink(args[0]))
+                {
+                    WriteLine("Downloading...");
+                    DownloadVideo(args[0], args[1], true);
+
+                    System.Console.WriteLine("Video is successfully downloaded!");
+                }
+                else
+                {
+                    WriteLine("An error occurred while providing the video link!");
+                }
+            }
+            else
+            {
+                WriteLine("An error occurred while entering the argument!");
+            }
+        }
+        static void DownloadVideo(string link, string output, bool openAfterDownload = false)
+        {
+            YouTube youtube = YouTube.Default;
+            YouTubeVideo video = youtube.GetVideo(link);
+
+            byte[] result = video.GetBytes();
+            string fileName = Path.Combine(output, $"{video.Title}.{video.FileExtension}");
+            File.WriteAllBytes(fileName, result);
+
+            if (openAfterDownload == true)
+            {
+                ProcessStartInfo info = new ProcessStartInfo();
+                info.FileName = fileName;
+                info.UseShellExecute = true;
+                Process.Start(info);
+            }
+        }
+        static bool isYouTubeVideoLink(string link)
+        {
+            Uri uri = new Uri(link);
+            return uri.Host == "www.youtube.com" || uri.Host == "wwww.youtu.be" ||
+                uri.Host == "youtube.com" || uri.Host == "youtu.be";
         }
     }
 }
